5487297ac4bced9a2a30135af7f5864420bb0243
Maria Luiza Cardoso <167557650+malu-car@users.noreply.github.com>
2024-04-19 13:39:00 -0300

    Lista de tarefas simples.
    Meu primeiro projeto com HTML, CSS e JavaScript.

---
+++ main.js
@@ -0,0 +1,14 @@
+function addNewTask() {
+    var list = document.getElementById("list");   
+    var text = document.getElementById("taskname").value;
+    if(text.length === 0) { 
+    alert('Adicione uma tarefa.');
+    return;
+    }
+    var listItem = document.createElement("li");
+    listItem.className = "list-item";
+    const textElement = document.createTextNode(text);
+    listItem.appendChild(textElement)
+    list.appendChild(listItem)
+    
+}
